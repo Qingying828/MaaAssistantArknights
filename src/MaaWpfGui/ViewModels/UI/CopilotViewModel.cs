2e6d7427616eb38a697f0187b7197dc1b1f5d24d
Qingying828 <2050300038@qq.com>
2026-07-09 01:34:33 +0800

    feat: gate owned operator precheck behind option

--- src/MaaWpfGui/ViewModels/UI/CopilotViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/CopilotViewModel.cs
@@ -459,6 +459,15 @@ public partial class CopilotViewModel : Screen
     /// </summary>
     public bool IgnoreRequirements { get => field; set => SetAndNotify(ref field, value); }
 
+    public bool UseOwnedOperPreCheck
+    {
+        get => field;
+        set {
+            SetAndNotify(ref field, value);
+            ConfigurationHelper.SetValue(ConfigurationKeys.CopilotUseOwnedOperPreCheck, value.ToString());
+        }
+    } = ConfigurationHelper.GetValue(ConfigurationKeys.CopilotUseOwnedOperPreCheck, false);
+
     /// <summary>
     /// Gets or sets a value indicating whether 真正有干员被忽略了要求
     /// </summary>
@@ -1998,7 +2007,7 @@ public partial class CopilotViewModel : Screen
 
     private async Task<bool> AppendAndStartCopilotAsync(IEnumerable<UserAdditional> userAdditional)
     {
-        var ownedOpers = GetOwnedOperIds();
+        var ownedOpers = UseOwnedOperPreCheck ? GetOwnedOperIds() : [];
         if (!UseCopilotList)
         {
         }
